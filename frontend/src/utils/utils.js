7dee5513f9714d5c626640c08882b7c7b52e5eba
hedgehogscodes <zjora.77rus@gmail.com>
2022-11-13 22:13:24 +0300

    Update utils.js

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,11 +1,11 @@
 export const apiOptions = {
-  baseUrl: 'https://mestoproject.student.nomoredomains.icu',
+  baseUrl: 'https://api.mestoproject.student.nomoredomains.icu',
   headers: {
     'Content-Type': 'application/json'
   }
 };
 
-export const baseUrl = 'https://mestoproject.student.nomoredomains.icu';
+export const baseUrl = 'https://api.mestoproject.student.nomoredomains.icu';
 
 export const checkResponse = (res) =>
   res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
